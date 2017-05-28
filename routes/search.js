dfdef6fd82fa24d19b65815d1f8cb82edd4d1adb
Josh <jjmoona0@gmail.com>
2017-05-29 03:06:34 +1200

    Added backend search functionality and fixed database connection

--- routes/search.js
+++ routes/search.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var database = "postgres://tivngnhwlxtmkp:4f2f1fff9cc8065295ac874e18ddd8d9f322f536cc50c9aea6610ce4002d3cea@ec2-23-21-169-238.compute-1.amazonaws.com:5432/d6ns5drql89lm1";
-var pg = require('pg');
+var pg = require('pg').native;
 /* GET users listing. */
 
 //TODO
 router.get('/', function(req, res, next) {
+  console.log(process.env.DATABASE_URL);
   console.log(req.params.sometime);
   //console.log(req.query.paramName);
   console.log(req.query);
@@ -17,19 +18,43 @@ router.get('/', function(req, res, next) {
 //Tag can either be a brand , style or location for now. Will be changed later given the time
 router.get('/:tag', function(req, res, next) {
 
-  console.log(req.params.tag);
-  var tag = req.params.tag;
-  //Connect to the database and do query search for the tag.
-  // pg.connect(database, function(err, client, done){
-
-  //   // client.query(the query, function(err, res){
-
-  //   // });
-  // });
-
-  res.render('search', 
+  var tags = req.params.tag;
+  var type= req.query.e;
+  console.log(tags);
+  console.log(type);
+  var searchResults = [];
+  pg.connect(database, function(err, client, done){
+    if(err){
+      throw err;
+    }
+    client.query("select * from items where "+tags+"='"+type+"';", function(err, result){
+      if(err){
+       res.status(500).send("Selection does not exist");
+       done();
+      }
+      else{
+        //TODO
+        searchResults = result.rows;
+        console.log(searchResults);
+        res.render('search', 
           { title: 'Search Page',
-            tag: tag});
+            tag: type});  
+      }
+    });
+
+  });
+
+  // if (tags == "style"){
+  //   // Connect to the database and do query search for the tag.
+  //   type = req.query.sty;
+  //   console.log(type);
+  //   pg.connect(database, function(err, client, done){
+
+  //     client.query("select from items where style='"+type"';", function(err, res){
+
+  //     });
+
+  //   });
 });
 
 module.exports = router;
