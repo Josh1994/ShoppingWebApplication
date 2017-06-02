a179b3133a7c911e237954d05b5c883605aa97c4
CoreyNZ <CoreyWilkinsonNZ@gmail.com>
2017-06-02 12:56:47 +1200

    progress with login, not completed

--- routes/login.js
+++ routes/login.js
@@ -3,27 +3,42 @@ var router = express.Router();
 var database = "postgres://tivngnhwlxtmkp:4f2f1fff9cc8065295ac874e18ddd8d9f322f536cc50c9aea6610ce4002d3cea@ec2-23-21-169-238.compute-1.amazonaws.com:5432/d6ns5drql89lm1";
 var pg = require('pg').native;
 
-
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login Page' });
 });
 
-/* Work in progress .....
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
 //PUT request for login
-app.put('/login', function(req, res){
+router.put('/', function(req, res){
 
   var username = req.body.user;
-  var password = req.body.password;
+  var pwd = req.body.password;
+  console.log(req.body.user);
+  console.log(req.body.password);
+
 
   pg.connect(database, function(err, client, done){
     if(err){
       throw err;
     }
 
-  client.query("select * from users ")
+    client.query("SELECT * FROM users WHERE email = '" +username+  '"AND password = "' +pwd+
+    function(error, result){
+      if(error){
+        res.status(500).send("Incorrect Password");
+        done();
+      } else {
+        done();
+        }
+    });
+  });
+
 
 
 });
-*/
+
 
 module.exports = router;
