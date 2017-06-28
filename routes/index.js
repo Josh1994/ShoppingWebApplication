510d3adc83f4e715d3910a94a29fab37a2b1e3a3
CoreyNZ <CoreyWilkinsonNZ@gmail.com>
2017-06-29 11:04:04 +1200

    Resolving failed lookup when logging in

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,8 @@ router.post('/', function(req, res){
       else {
         console.log(result);
         done();
-        res.render('Search', { title: 'Search Page' });
+        res.render('search', { title: 'Search Page' });
+
         /* Not currently working, giving error : Cannot read property 'email' of undefined
         res.render('/', { title: 'Main Page',
                               user: result[0].email });
